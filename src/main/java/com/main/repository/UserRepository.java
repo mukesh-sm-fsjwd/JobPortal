e48a394963e520f9a777bb9daa1682fc3e730f43
Mukesh SM <smmukesh.ai369@gmail.com>
2026-07-27 15:27:16 +0530

    Added UserRepository.

---
+++ src/main/java/com/main/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.main.repository;
+
+import com.main.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users,Integer> {
+    Optional<Users> findByUsername(String username);
+}
